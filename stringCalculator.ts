3f1252ae8b3d240909b4de8679cabc96c3e5d8e3
Tufail Shah <shahtuffey@gmail.com>
2024-05-03 21:42:27 +0530

    multiple numbers

--- stringCalculator.ts
+++ stringCalculator.ts
@@ -1,6 +1,7 @@
 export class StringCalculator {
-  add(numbers: string): number {
-    return parseInt(numbers);
-  }
+    add(numbers: string): number {
+      const numbersArray = numbers.split(',');
+      return numbersArray.reduce((sum, current) => sum + parseInt(current), 0);
+    }
 }
   
