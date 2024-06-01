06b545807ffeebd19d7be6604a6693d0cb2ba255
Tufail Shah <shahtuffey@gmail.com>
2024-06-01 21:34:25 +0530

    Update stringCalculator.rb and add negative check and pass basic tests

--- stringCalculator.rb
+++ stringCalculator.rb
@@ -1,8 +1,13 @@
-def add(numbers)
+class StringCalculator
+  MAX_SAFE_INTEGER = 9007199254740991
+
+  def add(numbers)
     return 0 if numbers.empty?
 
     delimiters = get_delimiters(numbers)
     numbers_array = numbers.split(Regexp.union(delimiters))
+
+    check_for_negatives(numbers_array)
 
     numbers_array.reduce(0) do |sum, current|
       num = current.to_i
@@ -11,9 +16,15 @@ def add(numbers)
     end
   end
 
-private
+  private
 
   def get_delimiters(numbers)
     custom_delimiter_match = numbers.match(/\/\/(.*)\n/)
     custom_delimiter_match ? custom_delimiter_match[1].split('') : [',', '\n']
   end
+
+  def check_for_negatives(numbers)
+    negative_numbers = numbers.select { |num| num.to_i < 0 }
+    raise "Negative numbers not allowed: #{negative_numbers.join(', ')}" unless negative_numbers.empty?
+  end
+end
