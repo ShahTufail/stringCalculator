9098d8ca79c7aeb5e5d7140e664ab92c2757c5e3
Tufail Shah <shahtuffey@gmail.com>
2024-06-01 21:26:59 +0530

    Update string_calculator_spec.rb

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -1,9 +1,9 @@
 
 
-// have used TDD approach to write test cases and include features in the code after failing and test case and then writing code to pass the test case.
-// tried to follow as per tdd appraoch given in the link,
+#  have used TDD approach to write test cases and include features in the code after failing and test case and then writing code to pass the test case.
+#  tried to follow as per tdd appraoch given in the link,
 
-// importing rSpec
+#  importing rSpec
 
 require 'rspec'
 require_relative 'string_calculator'
