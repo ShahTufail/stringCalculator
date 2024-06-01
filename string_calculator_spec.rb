5a66de31284a65423ae696367b043ba73bb02982
Tufail Shah <shahtuffey@gmail.com>
2024-06-01 21:36:58 +0530

    Update string_calculator_spec.rb

--- string_calculator_spec.rb
+++ string_calculator_spec.rb
@@ -7,13 +7,13 @@
 
 require 'rspec'
 require_relative 'string_calculator'
-# initialse and start with empty string test case
- RSpec.describe StringCalculator do
+
+RSpec.describe StringCalculator do
   it 'returns 0 for an empty string' do
     calculator = StringCalculator.new
     expect(calculator.add('')).to eq(0)
   end
-# test cases on delimiter and single digit
+
   it 'returns the single number' do
     calculator = StringCalculator.new
     expect(calculator.add('1')).to eq(1)
@@ -23,7 +23,8 @@ require_relative 'string_calculator'
     calculator = StringCalculator.new
     expect(calculator.add('1,2')).to eq(3)
   end
-    it 'adds multiple numbers separated by newlines' do
+
+  it 'adds multiple numbers separated by newlines' do
     calculator = StringCalculator.new
     expect(calculator.add('1\n2')).to eq(3)
   end
@@ -32,4 +33,24 @@ require_relative 'string_calculator'
     calculator = StringCalculator.new
     expect(calculator.add('//;\n1;2')).to eq(3)
   end
- end
+
+  it 'throws an exception for negative numbers' do
+    calculator = StringCalculator.new
+    expect { calculator.add('-1') }.to raise_error('Negative numbers not allowed: -1')
+  end
+
+  it 'handles numbers larger than max safe integer' do
+    calculator = StringCalculator.new
+    expect(calculator.add('9007199254740991')).to eq(9007199254740991) # Max safe integer in JS
+  end
+
+  it 'ignores numbers after exceeding max safe integer' do
+    calculator = StringCalculator.new
+    expect(calculator.add('1,9007199254740991,2')).to eq(9007199254740993) # Ignores the last number
+  end
+
+  it 'throws an exception for empty string after custom delimiters definition' do
+    calculator = StringCalculator.new
+    expect { calculator.add('//;') }.to raise_error('Numbers are required')
+  end
+end
